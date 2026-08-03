db11d5f2b3ef34bf44844fff693f85d26f0bb754
Andrey <susluk93@mail.ru>
2026-08-03 16:31:12 +0300

    Changed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', 
+  images: {
+    unoptimized: true, 
+  },
+  basePath: '/habit-tracker_Next_TS',
+  assetPrefix: '/habit-tracker_Next_TS/',
 };
 
 export default nextConfig;
