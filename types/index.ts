afe04b06f74291c0d5223ef6b40c3d5198fca7ba
Andrey <susluk93@mail.ru>
2026-08-03 21:29:39 +0300

    Updates

--- types/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Habit {
-  id: string;
-  name: string;
-  description?: string;
-  frequency: 'daily' | 'weekly';
-  streak: number;
-  createdAt: Date;
-  completions: Record<string, boolean>;
-}
